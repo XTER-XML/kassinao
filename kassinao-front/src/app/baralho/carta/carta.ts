18ceebed4a571aa5e8d1e50be32aa4b11a074694
XTER.xml <XTER_XML@outlook.com>
2022-03-11 20:24:19 -0300

    [Front] Created Carta component and class

---
+++ kassinao-front/src/app/baralho/carta/carta.ts
@@ -0,0 +1,11 @@
+class Carta {
+    numberValue: number;
+    cardType: string;
+    color: string;
+
+    constructor(numberValue:number, cardType:string, color:string){
+        this.numberValue = numberValue;
+        this.cardType = cardType;
+        this.color = color;
+    }
+}
